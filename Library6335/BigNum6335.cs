870a9c0feefc7a476d9a9d78181347727a92a8dd
Wuyiz <wuyizG@gmail.com>
2019-05-03 16:13:25 +0800

    修改了stylecop代码走审规则：方法名首字母大写

--- Library6335/BigNum6335.cs
+++ Library6335/BigNum6335.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Drawing;
-using System.Text;
+﻿namespace Library6335 {
+    using System;
+    using System.Drawing;
+    using System.Text;
 
-namespace Library6335 {
     public class BigNum6335 {
         private string[] bits = { "分", "角", "元", "拾", "佰", "千", "万", "十万", "佰万", "仟万", "亿" };
         private string[] nums = { "零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖" };
-        private decimal v;
 
         public Color Color {
             get {
@@ -17,17 +16,18 @@ namespace Library6335 {
                 }
             }
         }
+
         public decimal Money { get; set; }
 
-        public BigNum6335() {
+        //public BigNum6335() {
             //Money = Math.Round(Money, 2, MidpointRounding.AwayFromZero);    //四舍五入
-        }
+        //}
 
         //public BigNum6335(decimal money) {
         //    Money = Math.Round(Money, 2, MidpointRounding.AwayFromZero);    //四舍五入
         //}
 
-        private string getIntString(string intString) {
+        private string GetIntString(string intString) {
             StringBuilder stringBuilder = new StringBuilder(intString);
 
             int iTemp = stringBuilder.Length;  //获取整数部分位数
@@ -40,25 +40,25 @@ namespace Library6335 {
 
             //转换整数部分数字，i从位数开始
             for (int i = iTemp; i > 0; i--) {
-                Int32 num = Convert.ToInt32(stringBuilder[iTemp - i].ToString());   //获取当前数字
+                int num = Convert.ToInt32(stringBuilder[iTemp - i].ToString());   //获取当前数字
                 stringBuilder.Append(nums[num]);    //追加大写数字
                 stringBuilder.Append(bits[i + 1]);  //追加金额单位
             }
 
-            stringBuilder.Remove(0,iTemp);  //移除原有的小写数字内容
+            stringBuilder.Remove(0, iTemp);  //移除原有的小写数字内容
             intString = "" + stringBuilder;
-            
+
             return intString;
         }
 
-        private string getDotString(string dotString) {
-            StringBuilder stringBuilder = new StringBuilder(dotString); 
-            
+        private string GetDotString(string dotString) {
+            StringBuilder stringBuilder = new StringBuilder(dotString);
+
             dotString = ""; //初始化string
 
             //转换小数部分数字
             for (int i = 2; i > 0; i--) {
-                Int32 num = Convert.ToInt32(stringBuilder[2 - i].ToString());   //获取当前数字
+                int num = Convert.ToInt32(stringBuilder[2 - i].ToString());   //获取当前数字
                 stringBuilder.Append(nums[num]);    //追加大写数字
                 stringBuilder.Append(bits[i - 1]);  //追加金额单位
             }
@@ -79,22 +79,24 @@ namespace Library6335 {
             int index = money.ToString().IndexOf(".");
             if (index >= 0) {
                 string[] str = money.ToString().Split('.');
-                @int = getIntString(str[0]);
+                @int = GetIntString(str[0]);
+
                 //小数点后不足两位则补齐0
                 if (str[1].Length == 1) {
                     str[1] += "0";
-                    dot = getDotString(str[1]);
+                    dot = GetDotString(str[1]);
                 } else {
-                    dot = getDotString(str[1]);
+                    dot = GetDotString(str[1]);
                 }
             } else {
                 /* 若输入：
                  * 123. 或 123 这类没有小数点或小数点后无数字的金额时，补0
                  * 因为decimal会自动忽略没有尾数的数字小数点
                  */
-                @int = getIntString(money.ToString());
-                dot = getDotString("00");
+                @int = GetIntString(money.ToString());
+                dot = GetDotString("00");
             }
+
             return $"{@int}{dot}";
         }
     }
--- Win6335/OOP6335/BigNumForm6335.cs
+++ Win6335/OOP6335/BigNumForm6335.cs
@@ -12,10 +12,14 @@ using Library6335;
 namespace Win6335.OOP6335 {
     public partial class BigNumForm6335 : Form {
 
+        public BigNumForm6335() {
+            InitializeComponent();
+        }
+
         private void btnTrans_Click(object sender, EventArgs e) {
             try {
                 if (tbMoney.Text.Trim() != string.Empty) {
-                    string money = tbMoney.Text.Trim().Replace(" ", "");    //删除输入内容两边和之间存在的空格
+                    string money = tbMoney.Text.Replace(" ", "");    //删除输入内容两边和之间存在的空格
                     BigNum6335 bigNum = new BigNum6335();
                     bigNum.Money = Convert.ToDecimal(money);
                     lbUpper.ForeColor = bigNum.Color;
