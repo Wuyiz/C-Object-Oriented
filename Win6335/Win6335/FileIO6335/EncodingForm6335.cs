02d82358774c45cd8d070a0482bc5b11181b21d4
yizhang wu <wuyiz@foxmail.com>
2019-05-30 17:30:38 +0800

    创建了编码文件流的写入和读取

--- Win6335/Win6335/FileIO6335/EncodingForm6335.cs
+++ Win6335/Win6335/FileIO6335/EncodingForm6335.cs
@@ -13,13 +13,13 @@ namespace Win6335.Win6335.FileIO6335 {
     public partial class EncodingForm6335 : Form {
         public EncodingForm6335() {
             InitializeComponent();
-            cmbEncoding.Items.AddRange(new object[] {
-                "System.Text.DBCSCodePageEncoding",
-                "System.Text.ASCIIEncoding",
-                "System.Text.UnicodeEncoding",
-                "System.Text.UTF32Encoding",
-                "System.Text.UTF7Encoding",
-                "System.Text.UTF8Encoding"
+            cmbEncoding.Items.AddRange(new Encoding[] {
+                Encoding.Default,
+                Encoding.ASCII,
+                Encoding.Unicode,
+                Encoding.UTF32,
+                Encoding.UTF7,
+                Encoding.UTF8
             });
             cmbEncoding.SelectedIndex = 0;
             tbInput.Text = $@"使用{cmbEncoding.Text}，该类为抽象类";
@@ -27,17 +27,18 @@ namespace Win6335.Win6335.FileIO6335 {
 
         }
 
-
         private void btnWrite_Click(object sender, EventArgs e) {
             if (String.IsNullOrEmpty(tbFile_name.Text.Trim())) {
                 MessageBox.Show("请设置写入文件名称!");
                 return;
             }
 
+            //获取下拉列表中选中的编码格式
+            Encoding encoding = (Encoding)cmbEncoding.SelectedItem;
             //创建FileStream
             FileStream fsWrite = new FileStream(tbFile_name.Text, FileMode.OpenOrCreate, FileAccess.Write);
             //得到字节数组
-            byte[] buffer = Encoding.GetEncoding("utf-8").GetBytes(tbInput.Text);
+            byte[] buffer = encoding.GetBytes(tbInput.Text);
             //写入文件
             fsWrite.Write(buffer, 0, buffer.Length);
             //关闭流
@@ -46,19 +47,20 @@ namespace Win6335.Win6335.FileIO6335 {
             fsWrite.Dispose();
 
             //显示文件目录
-            MessageBox.Show($"文件位置：{Environment.CurrentDirectory}\\{tbFile_name.Text}");
+            //MessageBox.Show($"文件位置：{Environment.CurrentDirectory}\\{tbFile_name.Text}");
         }
 
         private void btnRead_Click(object sender, EventArgs e) {
             //创建FileStream
             using (FileStream fsRead = new FileStream(tbFile_name.Text, FileMode.OpenOrCreate, FileAccess.Read)) {
                 byte[] buffer = new byte[50];
-                int fsLen = fsRead.Read(buffer, 0, buffer.Length);
+                int fsLen = 0;
                 tbDisplay.Clear();
 
+                Encoding encoding = (Encoding)cmbEncoding.SelectedItem;
                 //从文件中读取字节数组
-                while (fsLen != 0) {
-                    tbDisplay.Text += Encoding.GetEncoding("UTF-8").GetString(buffer, 0, fsLen);
+                while ((fsLen = fsRead.Read(buffer, 0, buffer.Length)) != 0) {
+                    tbDisplay.Text += encoding.GetString(buffer, 0, fsLen);
                 }
             }
         }
